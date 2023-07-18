e574a7a0a0bdff06217acb4b8a11ec2bd4458440
Md Main Uddin <2021md.main@gmail.com>
2023-07-18 17:46:22 +0600

    book api fixes

--- dist/module/house/house.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { houseController } from './house.controller.js';
-import { listHouseValidator } from './house.validaton.js';
-import { verifyOwner } from '../../authHelper/verifyAuth.js';
-const router = express.Router();
-router.route('/list-house').post(verifyOwner, listHouseValidator, houseController.listAHouse);
-export default {
-  houseRouter: router
-};
